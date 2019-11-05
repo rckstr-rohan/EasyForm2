780590b513ec0d869b21b7fa47342e5da3c847f7
rckstr-rohan <56765439+rckstr-rohan@users.noreply.github.com>
2019-11-06 03:04:55 +0530

    no error

---
+++ app/src/main/java/com/example/easyform/HomeActivity.java
@@ -0,0 +1,17 @@
+//HomeActivity.java
+
+
+package com.example.easyform;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
